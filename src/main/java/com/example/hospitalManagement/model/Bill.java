3cb8a93d427123f46a4b2b2ef55c27aa11fcdac4
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2026-01-20 11:11:41 +0530

    models for bill

---
+++ src/main/java/com/example/hospitalManagement/model/Bill.java
@@ -0,0 +1,25 @@
+package com.example.hospitalManagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Bill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private long patientId;
+    private double amount;
+    private String status;
+
+
+}
