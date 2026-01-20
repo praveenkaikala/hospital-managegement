c22a28340119930e37d58178753e4d10d817cefb
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2026-01-20 11:09:44 +0530

    models for doctor

--- src/main/java/com/example/hospitalManagement/model/Doctor.java
+++ src/main/java/com/example/hospitalManagement/model/Doctor.java
@@ -18,6 +18,5 @@ public class Doctor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String gender;
-    private int age;
+    private String speciality;
 }
